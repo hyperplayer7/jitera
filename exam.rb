bb86587fc61ff4b3f55376ed26fb94a2d607fa03
hyperplayer7 <hyperplayer7@yahoo.com>
2023-02-21 12:29:55 +0800

    test connection

--- exam.rb
+++ exam.rb
@@ -19,3 +19,5 @@ print_usage(2)
 array_exam = ["B$u$i$ld", "$t$$h$e", "N$e$x$t", "E$$ra", "$$o$f$", "S$$of$t$wa$r$e", "De$$ve$l$op$me$n$t"]
 (array_exam*" ").gsub("$","").upcase
 print_usage(3)
+
+#test
